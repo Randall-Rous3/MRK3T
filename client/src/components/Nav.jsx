2cee8802a46ffb0a66a63ea0b7ab9671e11d1f9d
Randall Rouse <randall.g.rouse@gmail.com>
2022-02-12 15:40:55 -0500

    conditional Nav bar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,22 +3,37 @@ import { Link } from 'react-router-dom'
 const Nav = (props) => {
 const authenticated = props.authenticated
 
+if( authenticated){
 return(
 <div>
     <nav>
         <Link className="nav" to='/'>MRK3T</Link>
-        <Link className="nav" to='/register'>Create Account</Link>
-        <Link className="nav" to='/login'>Login</Link>
         <Link className="nav" onClick={props.handleLogOut} to="/">Logout</Link>
         <Link className="nav" to='/food'>Food</Link>
         <Link className="nav" to='/bev'>Bevreages</Link>
         <Link className="nav" to='/wares'>Wares</Link>
         <Link className="nav" to='/dashboard'>My account</Link>
-        <Link className="nav" to='/buggy'>Buggy</Link>
 
 
     </nav>
 </div>
 )
+} else {
+    return(
+        <div>
+    <nav>
+        <Link className="nav" to='/'>MRK3T</Link>
+        <Link className="nav" to='/register'>Create Account</Link>
+        <Link className="nav" to='/login'>Login</Link>
+        <Link className="nav" to='/food'>Food</Link>
+        <Link className="nav" to='/bev'>Bevreages</Link>
+        <Link className="nav" to='/wares'>Wares</Link>
+
+
+    </nav>
+</div>
+
+    )
+}
 }
 export default Nav
